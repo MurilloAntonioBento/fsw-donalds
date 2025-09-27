5e239e025b1afc7548d1d69b20dd3ed79e965894
Murillo <murilloantoniobento@gmail.com>
2025-09-27 17:33:22 -0300

    feat: add initial cart context and cart  sheet components

--- src/app/[slug]/menu/[productId]/page.tsx
+++ src/app/[slug]/menu/[productId]/page.tsx
@@ -1,10 +1,8 @@
-import { ChevronLeftIcon, ScrollTextIcon } from "lucide-react";
-import Image from "next/image";
 import { notFound } from "next/navigation";
 
-import { Button } from "@/components/ui/button";
 import { db } from "@/lib/prisma";
 
+import ProductDetails from "./components/product-details";
 import ProductHeader from "./components/product-header";
 
 interface ProductPageProps {
@@ -13,17 +11,28 @@ interface ProductPageProps {
 
 const ProductPage = async ({ params }: ProductPageProps) => {
   const { slug, productId } = await params;
-  const product = await db.product.findUnique({where: {id: productId}})
+
+  const product = await db.product.findUnique({where: {id: productId}, include: {
+    restaurant: {
+      select: {
+        name: true,
+        avatarImageUrl: true,
+        slug: true,
+      },
+    },
+  },
+})
   if (!product) {
     return notFound();
   }
+  if (product.restaurant.slug.toUpperCase() !== slug.toUpperCase()) {
+    return notFound();
+  }
   return (
-  <>
-  <ProductHeader product={product}/>
-  <h1>product page</h1>
-  {slug}
-  {productId}
-  </>
+  <div className="flex h-full flex-col">
+    <ProductHeader product={product}/>
+    <ProductDetails product={product}/>
+  </div>
     
   );
 };
