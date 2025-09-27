f5ad767b9b87dd0eacfea3ee766c7a63dd941938
Murillo <murilloantoniobento@gmail.com>
2025-09-27 17:43:53 -0300

    feat: add initial add to cart function to cart context

--- src/app/[slug]/menu/components/cart-sheet.tsx
+++ src/app/[slug]/menu/components/cart-sheet.tsx
@@ -1,24 +1,35 @@
 import { useContext } from "react";
 
-import { Sheet, SheetContent, SheetDescription, SheetHeader, SheetTitle } from "@/components/ui/sheet";
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+} from "@/components/ui/sheet";
 
 import { CartContext } from "../contexts/cart";
 
 const CartSheet = () => {
-  const {isOpen, toggleCart} = useContext(CartContext)
-  return ( 
+  const { isOpen, toggleCart, products } = useContext(CartContext);
+  return (
     <Sheet open={isOpen} onOpenChange={toggleCart}>
-        <SheetContent>
-          <SheetHeader>
-            <SheetTitle>Título</SheetTitle>
-            <SheetDescription>
-              Descrição
-            </SheetDescription>
+      <SheetContent>
+        <SheetHeader>
+          <SheetTitle>Are you absolutely sure?</SheetTitle>
+          <SheetDescription>
+            This action cannot be undone. This will permanently delete your
+            account and remove your data from our servers.
+          </SheetDescription>
+        </SheetHeader>
+        {products.map((product) => (
+          <h1 key={product.id}>
+            {product.name} - {product.quantity}
+          </h1>
+        ))}
+      </SheetContent>
+    </Sheet>
+  );
+};
 
-          </SheetHeader>
-        </SheetContent>
-      </Sheet>
-   );
-}
- 
 export default CartSheet;
